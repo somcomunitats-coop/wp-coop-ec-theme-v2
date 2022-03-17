2408f947a9b0fe11cf60c72efaf5d5df3c44de07
Dani Quilez <dani@daniquilez.com>
2022-03-17 15:57:49 +0100

    base setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 const _ = require("lodash");
 const theme = require('./theme.json');
 const tailpress = require("@jeffreyvr/tailwindcss-tailpress");
+const parent_theme = require('../wp-coop-theme/theme.json');
+const parent_tailwind_config = require('../wp-coop-theme/tailwind.config.js');
+
+function get_variable_value(variable){
+  value = tailpress.theme(variable, theme);
+  if(value){
+    return value;
+  }else{
+    value = tailpress.theme(variable, parent_theme);
+    if(value){
+      return value;
+    }
+  }
+  return false;
+}
 
 module.exports = {
   content: [
+    '../wp-coop-theme/*/*.php',
+    '../wp-coop-theme/**/*.php',
     './*/*.php',
     './**/*.php',
     './resources/css/*.css',
@@ -19,15 +36,15 @@ module.exports = {
       },
     },
     extend: {
-      colors: tailpress.colorMapper(tailpress.theme('settings.color.palette', theme)),
-      fontSize: tailpress.fontSizeMapper(tailpress.theme('settings.typography.fontSizes', theme))
+      colors: tailpress.colorMapper(get_variable_value('settings.color.palette')),
+      fontSize: tailpress.fontSizeMapper(get_variable_value('settings.typography.fontSizes'))
     },
     screens: {
       'xs': '480px',
       'sm': '600px',
       'md': '782px',
-      'lg': tailpress.theme('settings.layout.contentSize', theme),
-      'xl': tailpress.theme('settings.layout.wideSize', theme),
+      'lg': get_variable_value('settings.layout.contentSize'),
+      'xl': get_variable_value('settings.layout.wideSize'),
       '2xl': '1440px'
     }
   },
