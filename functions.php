d4e40e1875b78979e1a7a74709bec35c50f30e5e
Dani Quilez <dani@daniquilez.com>
2022-07-04 15:42:46 +0200

    responsive breakpoints integration

--- functions.php
+++ functions.php
@@ -11,8 +11,10 @@ add_action( 'after_setup_theme', 'wpct_child_setup' );
  */
 function wp_coop_theme_child_enqueue_scripts() {
   wp_dequeue_style('wp-coop-theme');
+  wp_dequeue_style('wp-coop-theme-responsive');
   $theme = wp_get_theme();
   wp_enqueue_style( 'wp-coop-theme-child', get_stylesheet_directory_uri().'/css/app.css' , array(), $theme->get( 'Version' ) );
+  wp_enqueue_style( 'wp-coop-theme-child=responsive', get_stylesheet_directory_uri().'/css/responsive.css' , array(), $theme->get( 'Version' ) );
   wp_enqueue_script( 'wp-coop-theme-child', get_stylesheet_directory_uri(). '/js/app.js' , array('wp-coop-theme'), $theme->get( 'Version' ) );
 
 }
