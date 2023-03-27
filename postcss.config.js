e8b3e834f7d0a01833f38858a276c6a2b5550c21
Lucas García <lucas@codeccoop.org>
2023-03-27 13:21:23 +0200

    feat: update parent-theme style imports and apply css bundle

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   plugins: [
-    require('postcss-nested-ancestors'),
-    require('postcss-nested'),
-    require('postcss-import'),
-    require('tailwindcss')
-  ]
-}
+    require("postcss-import"),
+    require("postcss-nested-ancestors"),
+    require("postcss-nested"),
+    require("tailwindcss"),
+  ],
+};
+
