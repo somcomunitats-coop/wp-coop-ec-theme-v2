b6965a79bdf04ec7f82258ed17bda30bac5a85b2
Pau <pau.pulidocompanys@gmail.com>
2023-03-28 11:54:49 +0200

    fix: import fonts using base64 url

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
+    require("postcss-url")({
+      url: 'inline'
+    }),
     require("postcss-nested-ancestors"),
     require("postcss-nested"),
     require("tailwindcss"),
