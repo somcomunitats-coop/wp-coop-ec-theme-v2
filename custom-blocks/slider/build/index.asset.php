b4d3ff921d53cb0778d324f089b679be3ae9c72b
Lucas García <lucas@codeccoop.org>
2023-07-14 14:05:48 +0200

    feat: build slider custom block

--- custom-blocks/slider/build/index.asset.php
+++ custom-blocks/slider/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '6a62cd8f3333878e1338');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '30580a4e73507a92defb');
