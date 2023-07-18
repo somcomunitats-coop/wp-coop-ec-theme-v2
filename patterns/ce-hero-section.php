82a0900fb591b16f89a84d540c219c847a0fff6d
Lucas García <lucas@codeccoop.org>
2023-07-18 15:19:30 +0200

    feat: section patterns

---
+++ patterns/ce-hero-section.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * Title: CE Hero Section
+ * Slug: ce-pattern/hero-section
+ * Category: ce-pattern
+ * Viewport Width: 1500
+ */
+
+?>
+
+<!-- wp:group {"align":"full","backgroundColor":"main","className":"is-style-no-padding","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull is-style-no-padding has-main-background-color has-background"><!-- wp:group {"className":"is-style-horizontal-padded","layout":{"type":"constrained"}} -->
+  <div class="wp-block-group is-style-horizontal-padded"><!-- wp:spacer {"height":"6rem","className":"is-style-show-desktop"} -->
+    <div style="height:6rem" aria-hidden="true" class="wp-block-spacer is-style-show-desktop"></div>
+    <!-- /wp:spacer -->
+
+    <!-- wp:spacer {"height":"3rem","className":"is-style-show-mobile-tablet"} -->
+    <div style="height:3rem" aria-hidden="true" class="wp-block-spacer is-style-show-mobile-tablet"></div>
+    <!-- /wp:spacer -->
+
+    <!-- wp:heading {"textAlign":"center","textColor":"base"} -->
+    <h2 class="wp-block-heading has-text-align-center has-base-color has-text-color">Títol de la secció</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"align":"center","textColor":"base","fontSize":"medium"} -->
+    <p class="has-text-align-center has-base-color has-text-color has-medium-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:spacer {"height":"2rem"} -->
+    <div style="height:2rem" aria-hidden="true" class="wp-block-spacer"></div>
+    <!-- /wp:spacer -->
+
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","fontSize":"small"} -->
+      <div class="wp-block-button has-custom-font-size has-small-font-size"><a class="wp-block-button__link has-text-align-center wp-element-button">Call to action</a></div>
+      <!-- /wp:button -->
+    </div>
+    <!-- /wp:buttons -->
+
+    <!-- wp:spacer {"height":"7rem","className":"is-style-show-desktop"} -->
+    <div style="height:7rem" aria-hidden="true" class="wp-block-spacer is-style-show-desktop"></div>
+    <!-- /wp:spacer -->
+
+    <!-- wp:spacer {"height":"4rem","className":"is-style-show-mobile-tablet"} -->
+    <div style="height:4rem" aria-hidden="true" class="wp-block-spacer is-style-show-mobile-tablet"></div>
+    <!-- /wp:spacer -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
