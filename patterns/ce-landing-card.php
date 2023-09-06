8f86f7f14350995f8ffa6c5e3e1912ff8f395b2a
Lucas García <lucas@codeccoop.org>
2023-09-06 14:54:42 +0200

    feat: apply rest landings layouts

--- patterns/ce-landing-card.php
+++ patterns/ce-landing-card.php
@@ -13,7 +13,7 @@
 <article class="wp-block-group is-style-no-padding has-base-background-color has-background ce-card ce-landing-card">
   <!-- wp:post-featured-image {"isLink":true,"align":"wide"} /-->
 
-  <!-- wp:post-terms {"term":"ce-status", "separator": ""} /-->
+  <!-- wp:post-terms {"term":"rest-ce-status", "separator": ""} /-->
 
   <!-- wp:spacer {"height":"40px"} -->
   <div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
@@ -23,19 +23,19 @@
   <div class="wp-block-group ce-card-body">
     <!-- wp:group {"className":"is-style-show-desktop"} -->
     <div class="wp-block-group is-style-show-desktop">
-      <!-- wp:post-terms {"term":"ce-eaction","separator":""} /-->
+      <!-- wp:post-terms {"term":"rest-ce-service","separator":""} /-->
     </div>
     <!-- /wp:group -->
 
     <!-- wp:group {"className":"is-style-show-tablet"} -->
     <div class="wp-block-group is-style-show-tablet">
-      <!-- wp:post-terms {"term":"ce-eaction","separator":""} /-->
+      <!-- wp:post-terms {"term":"rest-ce-service","separator":""} /-->
     </div>
     <!-- /wp:group -->
 
     <!-- wp:group {"className":"is-style-show-mobile"} -->
     <div class="wp-block-group is-style-show-mobile">
-      <!-- wp:post-terms {"term":"ce-eaction","separator":""} /-->
+      <!-- wp:post-terms {"term":"rest-ce-service","separator":""} /-->
     </div>
     <!-- /wp:group -->
 
@@ -43,8 +43,11 @@
     <div style="height:1em;width:0px" aria-hidden="true" class="wp-block-spacer"></div>
     <!-- /wp:spacer -->
     <!-- wp:post-date /-->
-    <!-- wp:post-title {"isLink":true,"level":4,"className":"wp-block-heading"} /-->
-    <!-- wp:post-excerpt {"moreText":"Més informació"} /-->
+    <!-- wp:group {"className":"ce-card-content"} -->
+    <div class="wp-block-group ce-card-content">
+      <!-- wp:post-title {"isLink":true,"level":4,"className":"wp-block-heading"} /-->
+      <!-- wp:post-excerpt {"moreText":"Més informació"} /-->
+    </div>
   </div>
   <!-- /wp:group -->
 
